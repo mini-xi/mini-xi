209818b53e8356957d3ae381e282f205a8224727
mini-xi <gam1205@naver.com>
2024-02-27 19:49:28 +0900

    no message

---
+++ 08_jpa/chap03-association-mapping-lecture-source/src/test/java/com/ohgiraffers/section01/manytoone/Category.java
@@ -0,0 +1,78 @@
+package com.ohgiraffers.section01.manytoone;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Objects;
+
+@Entity(name = "category_section01")
+@Table(name = "tbl_category")
+public class Category {
+
+    @Id
+    @Column(name = "category_code")
+    private int categoryCode;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @Column(name = "ref_category_code")
+    private Integer refCategoryCode;
+
+    public Category() {
+    }
+
+    public Category(int categoryCode, String categoryName, Integer refCategoryCode) {
+        this.categoryCode = categoryCode;
+        this.categoryName = categoryName;
+        this.refCategoryCode = refCategoryCode;
+    }
+
+    public int getCategoryCode() {
+        return categoryCode;
+    }
+
+    public void setCategoryCode(int categoryCode) {
+        this.categoryCode = categoryCode;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+
+    public Integer getRefCategoryCode() {
+        return refCategoryCode;
+    }
+
+    public void setRefCategoryCode(Integer refCategoryCode) {
+        this.refCategoryCode = refCategoryCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryCode=" + categoryCode +
+                ", categoryName='" + categoryName + '\'' +
+                ", refCategoryCode=" + refCategoryCode +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return categoryCode == category.categoryCode && Objects.equals(categoryName, category.categoryName) && Objects.equals(refCategoryCode, category.refCategoryCode);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(categoryCode, categoryName, refCategoryCode);
+    }
+}
